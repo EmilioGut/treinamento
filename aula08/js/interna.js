bd1f504c12640da2a26f2520f201c634f4b96561
EmersonPaduan <emerson.paduan@gmail.com>
2021-05-13 16:58:08 -0300

    aula8-final

--- aula08/js/interna.js
+++ aula08/js/interna.js
@@ -17,3 +17,42 @@ function logout() {
     localStorage.removeItem("userLogged")
     window.location = "index.html"
 }
+
+function listarUsuarios() {
+    fetch("http://localhost:8080/user/all")
+    .then(res => tratarRetorno(res))
+}
+
+function tratarRetorno(resposta) {
+    if(resposta.status == 200) {
+        // document.getElementById("tabela").innerHTML = "Usuários encontrados"
+        resposta.json().then( res => exibirUsuarios(res) )
+    } else {
+        document.getElementById("tabela").innerHTML = "Nenhum usuário encontrado"
+    }
+}
+
+function exibirUsuarios(listaUsuarios) {
+    let tabela = "<table> <tr> <th>Nome</th> <th>email</th> <th>CPF</th> </tr>"
+
+    for (let index = 0; index < listaUsuarios.length; index++) {
+        tabela = tabela + `<tr> <td>${listaUsuarios[index].nome}</td> 
+                            <td>${listaUsuarios[index].email}</td> 
+                            <td>${listaUsuarios[index].cpf}</td> </tr>`;
+    }
+
+    tabela = tabela + " </table>"
+    document.getElementById("tabela").innerHTML = tabela
+    
+    montarSelecao(listaUsuarios);
+}
+
+function montarSelecao(listaUsuarios) {
+    let selecao = "<select>"
+
+    for (let index = 0; index < listaUsuarios.length; index++) {
+        selecao = selecao + `<option>${listaUsuarios[index].nome}</option>`
+    }
+    selecao = selecao + "</select>" 
+    document.getElementById("selecao").innerHTML = selecao
+}
